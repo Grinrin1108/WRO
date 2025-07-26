1f603e1afa8b5cf69173a2764e6491ba2bd51b86
Grinrin <126931736+gurinrin@users.noreply.github.com>
2025-07-26 13:50:43 +0900

    save

---
+++ 07_26_0950.py
@@ -0,0 +1,673 @@
+import runloop
+import motor
+from hub import port
+from hub import light_matrix
+from time import sleep_ms as sleep
+import motor_pair
+import color_sensor
+import color
+from hub import sound
+
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+WHEEL = motor_pair.PAIR_1 #タイヤ
+NAKAHARA = port.C
+IMAI = port.E
+MV_DG = motor.run_for_degrees #角度を指定して進むやつ
+MV_P_DG = motor_pair.move_for_degrees #角度を指定して進むやつのペア
+SLEEP = runloop.sleep_ms
+
+#変数設定
+#モーターのスピード
+FAST = 1500
+MID = 700
+LOW = 500
+#角度
+D90 = 263
+D45 = 131
+#色
+colors = []
+
+#音楽
+l = 150
+
+c=523
+d=587
+e=659
+f=698
+f2 = 740
+g=784
+a=880
+a2=932
+b=988
+
+#かわいいだけじゃだめですか
+kawaii = [
+    (d, l),
+    (e, l),
+    (f2, l),
+    (g, l),
+    (a, l),
+    (g, l),
+    (f2, l),
+    (e, l),
+    (d, l),
+    (0, l),
+    (d*2, l),
+    (0, l),
+    (d, l),
+
+    #さび
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a,l),
+    (g, l*2),
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a, l),
+    (g, l*2),
+    (d, l),
+    (e, l),
+    (g, l),
+    (a, l),
+    (a, l),
+    (a, l),
+    (g, l),
+    (f2, l),
+    (c*2, l),
+    (b, l),
+    (g, l*2),
+    (g, l),
+    (a, l*2),
+    (b, l),
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a, l),
+    (g, l*2),
+    (d, l),
+    (d*2, l),
+    (d*2, l),
+    (d*2, l),
+    (c*2, l),
+    (b, l),
+    (g, l*2),
+    (a2, l),
+    (a, l),
+    (g, l),
+    (0, l),
+    (a2, l),
+    (a, l),
+    (g, l),
+    (g, l),
+    (d, l),
+    (a, l),
+    (g, l*5),
+
+    (0, l),
+
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a,l),
+    (g, l*2),
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a, l),
+    (g, l*2),
+    (d, l),
+    (e, l),
+    (g, l),
+    (a, l),
+    (a, l),
+    (a, l),
+    (g, l),
+    (f2, l),
+    (c*2, l),
+    (b, l),
+    (g, l*2),
+    (g, l),
+    (a, l*2),
+    (b, l),
+    (d, l),
+    (g, l),
+    (a, l),
+    (b, l),
+    (a2, l),
+    (a, l),
+    (g, l*2),
+    (d, l),
+    (d*2, l),
+    (d*2, l),
+    (d*2, l),
+    (c*2, l),
+    (b, l),
+    (g, l*2),
+    (a2, l),
+    (a, l),
+    (g, l),
+    (0, l),
+    (a2, l),
+    (a, l),
+    (g, l),
+    (a2, l),
+    (a, l),
+    (g, l),
+    (d*2, l*2),
+    (d*2, l),
+    (d*2, l),
+    (d*2, l),
+    (d*2, l),
+    (g, l),
+    (a, l),
+    (g, l*2),
+
+    (0, l*4)
+]
+
+async def play_melody():
+    for i in range(4):
+        for freq, dur in kawaii:
+            await sound.beep(freq, dur)# ← 修正：キーワード引数ではなく位置引数
+            await runloop.sleep_ms(50)# 少し休符
+
+#関数
+#まっすぐ進む
+async def straight(d, v):
+    await MV_P_DG(WHEEL, d, 0, velocity=v)
+    #await SLEEP(1000)
+    print("done straight!")
+
+#曲がる
+async def turn(d, v):
+    MV_DG(port.B, -d, v)
+    MV_DG(port.A, -d, v)
+    await SLEEP(500)
+    print("done turn!")
+
+#中原
+async def nakahara(d, v):
+    await MV_DG(NAKAHARA, d, v)
+    #await SLEEP(1000)
+    print("done nakahara!")
+
+#今井
+async def imai(d, v):
+    await MV_DG(IMAI, d, v)
+    print("done imai!")
+
+#色読み取り
+async def readColor():
+    #r, g, b, i = 0, 0, 0, 0
+
+    #for k in range(10):
+        #colors = color_sensor.rgbi(port.F)
+        #r += colors[0]
+        #g += colors[1]
+        #b += colors[2]
+        #i += colors[3]
+        #await SLEEP(100)
+    #r = r//10
+    #g = g//10
+    #b = b//10
+    #i = i//10
+    #print(r,g,b,i)
+    #色振り分け
+    #if r >= 100 and g >= 100:
+        #print("yellow")
+    #elif r >= g and r >= b:
+        #print("red")
+    #elif g >= r and g >= b:
+        #print("green")
+    #elif b >= r and b >= g:
+        #print("blue")
+    await SLEEP(500)
+    if color_sensor.color(port.F) is color.RED:
+        print("赤色だよ")
+        return "red"
+    elif color_sensor.color(port.F) is color.YELLOW:
+        print("黄色だよ")
+        return "yellow"
+    elif color_sensor.color(port.F) is color.BLUE:
+        print("青色だよ")
+        return "blue"
+    elif color_sensor.color(port.F) is color.GREEN:
+        print("緑色だよ")
+        return "green"
+
+    print("done readColor!")
+
+#ボルト場合分け
+async def boltCaseClassification():
+    former = -1
+    later = -1
+    #青黄の場合分け
+    if colors[2] == "blue": #青が２の時
+        return 1 #最適のやつ
+    elif colors[1] == "blue": #青が１の時
+        return 2 #ずらす
+    elif colors[2] == "yellow":
+        return 3 #縦に置く
+    else:
+        return 1
+    #赤緑の場合分け
+    print("done boltCaseClassification!")
+
+#テスト
+async def test():
+    #MV_DG(MOTOR1, 1000, FAST)
+    #motor.run(port.A, 1000)
+    await straight(-500, FAST)
+    await turn(-D90, MID)
+    #await straight(-500, FAST)
+    #await turn(526, MID)
+
+    print("done test!")
+
+#つなぎ0
+async def jumper0():
+    #初動
+    runloop.run(imai(-150, MID),straight(-350, LOW))
+    #壁あて
+    runloop.run(nakahara(-80, MID), straight(3650, FAST))
+    await turn(-D90, MID)
+    await straight(-250, LOW)
+    await straight(160, FAST)
+    await turn(D90, MID)
+    await straight(200, FAST)
+    print("done jumper0!")
+
+#赤色黄色棒
+async def stickProcess():
+    #オブジェクトまで行く
+    await straight(100, FAST)
+    #全部取る
+    await nakahara(-115, MID)
+    await SLEEP(500)
+    #下がって旋回
+    await straight(-430, MID)
+    await turn(-D90, MID)
+    #壁あて
+    await straight(-250, LOW)
+    #置き場まで行く
+    await straight(1450,FAST)
+    await turn(D90, MID)
+    #黄色を指定の場所にセット
+    await turn(D45, MID)
+    await straight(450, FAST)
+    #赤色だけをとる
+    await nakahara(115, MID)
+    await straight(-100, FAST)
+    await nakahara(-115, MID)
+    #下がる
+    await straight(-170, FAST)
+    #赤色を指定の場所にセット
+    await turn(-255, MID)
+    await straight(400, FAST)
+    await nakahara(115, MID)
+    print("done stickProcess!")
+
+#つなぎ１
+async def jumper1():
+    await straight(-1240, FAST)
+    await turn(-D45, MID)
+    print("done jumper1!")
+
+#引くやつ
+async def pullProcess():
+    #オブジェクトまで行く
+    await straight(300, FAST)
+    #引く
+    await nakahara(-90, MID)
+    await straight(-360, FAST)
+    await straight(50, FAST)
+    await nakahara(90, MID)
+    await straight(-50, FAST)
+    print("done pullProcess!")
+
+#つなぎ2
+async def jumper2():
+    #旋回
+    await turn(D90, MID)
+    #目的地に行く
+    await straight(-390, FAST)
+    await turn(D90, MID)
+    await straight(-1000, FAST)
+    await straight(-200, LOW)
+    print("done jumper2!")
+
+#ボルトと正方形の色を読み取る
+async def readColorProcess():
+    #読み取る
+    colors.append(await readColor())
+    #旋回
+    await straight(500, FAST)
+    await turn(D90, MID)
+    await straight(260, FAST)
+    await turn(D90, MID)
+    await straight(270, FAST)
+    #読み取る
+    colors.append(await readColor())
+    await straight(180, FAST)
+    colors.append(await readColor())
+    #color4を定義
+    if "red" not in colors:
+        colors.append("red")
+    elif "blue" not in colors:
+        colors.append("blue")
+    elif "yellow" not in colors:
+        colors.append("yellow")
+    elif "green" not in colors:
+        colors.append("green")
+    #正方形の色を読み取る
+    await straight(-820, FAST)
+    await MV_DG(port.B, 185, MID)
+    await MV_DG(port.A, -185, MID)
+    await MV_DG(port.B, -185, MID)
+    await MV_DG(port.A, 185, MID)
+    colors.append(await readColor())
+    print(colors)
+    print("done readColorProcess!")
+
+#つなぎ3
+async def jumper3():
+    #旋回
+    await straight(-700, FAST)
+    await turn(D90, MID)
+    #壁あて
+    await straight(-2000, FAST)
+    await straight(-300, LOW)
+
+#-----ここから分岐-----
+#正方形のオブジェクト
+#青の時
+async def sBlue():
+    #旋回
+    await straight(250, FAST)
+    await turn(-D90, MID)
+    await straight(-300, LOW)
+    await straight(505, FAST)
+    #とる
+    await nakahara(-115, MID)
+    #旋回
+    await straight(480, FAST)
+    await turn(D90, MID)
+    await straight(-300, LOW)
+
+    print("done sBlue")
+
+#赤の時
+async def sRed():
+    #旋回
+    await straight(700, FAST)
+    await turn(-D90, MID)
+    await straight(-300, LOW)
+    await straight(700, FAST)
+    await turn(-D90, MID)
+    await straight(400, FAST)
+    #とる
+    await nakahara(-110, MID)
+    #旋回
+    await turn(D90, MID)
+    await straight(280, FAST)
+    await turn(D90, MID)
+    await straight(-400, LOW)
+    print("done sRed!")
+
+#黄の時
+async def sYellow():
+    #旋回
+    await straight(700, FAST)
+    await turn(-D90, MID)
+    await straight(-300, LOW)
+    await straight(1100, FAST)
+    await turn(-D90, MID)
+    await straight(480, FAST)
+    await turn(-D90,FAST)
+    await straight(300, FAST)
+    #とる
+    await nakahara(-115, MID)
+    #旋回
+    await straight(-205, FAST)
+    await turn(-D90, MID)
+    await straight(-300, LOW)
+    print("done sYellow!")
+
+#緑色の時
+async def sGreen():
+    #旋回
+    await straight(250, FAST)
+    await turn(-D90, MID)
+    #壁あて
+    await straight(-300, LOW)
+    await straight(550, FAST)
+    #とる
+    await nakahara(-115, MID)
+    #旋回
+    await turn(D90, MID)
+    await straight(200, FAST)
+    #離す
+    await nakahara(110, MID)
+    #旋回
+    await straight(-200, FAST)
+    await turn(-D90, MID)
+    await straight(400, FAST)
+    await turn(D90, MID)
+    await straight(400, FAST)
+    await turn(D90, MID)
+    await straight(300, FAST)
+    #つかむ
+    await nakahara(-110, MID)
+    #旋回
+    await straight(-340, FAST)
+    await turn(-D90, MID)
+    await straight(-400, FAST)
+    await straight(-300, LOW)
+
+    print("done sGreen!")
+
+#正方形のオブジェクトメイン処理
+async def squareProcess():
+    if colors[4] == "red":
+        await sRed()
+    elif colors[4] == "blue":
+        await sBlue()
+    elif colors[4] == "yellow":
+        await sYellow()
+    elif colors[4] == "green":
+        await sGreen()
+    else:
+        await sBlue()
+    #置き場まで行く
+    await straight(1000, FAST)
+    await nakahara(110, MID)
+    await straight(560, FAST)
+    print("done squareProcess!")
+
+#旗
+async def flagProcess():
+    #旋回
+    await straight(-715, FAST)
+    await turn(D90, MID)
+    await straight(-365, FAST)
+    #タッグプレイ
+    await nakahara(-130, FAST)
+    await imai(160, 2000)
+    await imai(10, LOW)
+    await straight(300, FAST)
+    await nakahara(130, FAST)
+    #旋回
+    await turn(D90, MID)
+    await straight(-300, FAST)
+    await turn(D90, MID)
+    await straight(395, FAST)
+    #旗揚げ
+    await nakahara(-110, MID)
+    await straight(-160, FAST)
+    await straight(50, 50)
+    await nakahara(75, MID)
+    print("done flagProcess!")
+
+#つなぎ4
+async def jumper4():
+    #壁あて
+    await straight(-1300, FAST)
+    await straight(-200, LOW)
+    await straight(1000, FAST)
+    await turn(D90, MID)
+    await straight(-1100, FAST)
+    await straight(-200, LOW)
+    await straight(60, FAST)
+    await turn(D90, MID)
+    await imai(-160, FAST)
+    print("done jumper4!")
+
+#ボルト1
+async def bolt1():
+    #壁あて
+    await straight(200, FAST)
+    await turn(-D90, MID)
+    await straight(-100, LOW)
+    #旋回
+    await straight(1400, FAST)
+    await turn(D90, MID)
+    await straight(-400, FAST)
+    await straight(-100, LOW)
+    await straight(50, FAST)
+    await turn(D90, MID)
+    await imai(-160, FAST)
+    await straight(-210, FAST)
+    #ここから2個目
+    #旋回
+    await straight(210, FAST)
+    await turn(D90, MID)
+    await straight(-400, FAST)
+    await turn(D90, MID)
+    await straight(-1300, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    await straight(60, FAST)
+    await turn(D90, MID)
+    await straight(-380, FAST)
+    #つかむ
+    await imai(160, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    #旋回
+    await straight(1350, FAST)
+    await turn(-D90, MID)
+    await straight(-200, LOW)
+    await straight(2200, FAST)
+    #ゲート閉める
+    await closeProcess()
+    #旋回
+    await straight(1400, FAST)
+    await turn(-D90, MID)
+    await straight(-400, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    #旋回
+    await straight(1780, FAST)
+    await turn(D90, MID)
+    #置く
+    await straight(-250, FAST)
+
+    print("done bolt1")
+
+#ボルト2
+async def bolt2():
+    #壁あて
+    await straight(200, FAST)
+    await turn(-D90, MID)
+    await straight(-100, LOW)
+    #旋回
+    await straight(1400, FAST)
+    await turn(D90, MID)
+    await straight(-400, FAST)
+    await straight(-100, LOW)
+    await straight(220, FAST)
+    await turn(D90, MID)
+    await imai(-160, FAST)
+    await straight(-210, FAST)
+    #ここから2個目
+    #旋回
+    await straight(210, FAST)
+    await turn(D90, MID)
+    await straight(-400, FAST)
+    await turn(D90, MID)
+    await straight(-1300, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    await straight(60, FAST)
+    await turn(D90, MID)
+    await straight(-480, FAST)
+    #つかむ
+    await imai(160, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    #旋回
+    await straight(1350, FAST)
+    await turn(-D90, MID)
+    await straight(-200, LOW)
+    await straight(2200, FAST)
+    #ゲート閉める
+    await closeProcess()
+    #旋回
+    await straight(1400, FAST)
+    await turn(-D90, MID)
+    await straight(-400, FAST)
+    #壁あて
+    await straight(-200, LOW)
+    #旋回
+    await straight(1780, FAST)
+    await turn(D90, MID)
+    #置く
+    await straight(-250, FAST)
+    print("done bolt2!")
+
+#ボルト
+async def boltProcess():
+    #前に行く
+    await straight(-535, FAST)
+    #とる
+    await imai(180, FAST)
+    await bolt2()
+    print("done boltProcess!")
+
+#閉めるやつ
+async def closeProcess():
+    #ぶつける
+    await turn(-90, MID)
+    await turn(90, MID)
+    #通り過ぎるときに閉める
+    print("done closeProcess!")
+
+#メイン処理
+async def main():
+    #await test()
+    await jumper0()
+    await stickProcess()
+    await jumper1()
+    await pullProcess()
+    await jumper2()
+    await readColorProcess()
+    await jumper3()
+    await squareProcess()
+    await flagProcess()
+    await jumper4()
+    await boltProcess()
+    print("done all process!")
+
+runloop.run(main(), play_melody())
